6f6603a3a349ca3578370833b3642eb57b823093
Spence <103948370+yarospence@users.noreply.github.com>
2022-06-18 22:54:42 -0400

    IcToken
    Yield Farming domain Token
    
    Signed-off-by: Spence <103948370+yarospence@users.noreply.github.com>

---
+++ IcToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IcToken {
+    function mint(uint mintAmount) external returns (uint);
+    function redeem(uint redeemTokens) external returns (uint);
+    function borrow(uint borrowAmount) external returns (uint);
+    function repayBorrow(uint repayAmount) external returns (uint);
+    function borrowBalanceCurrent(address account) external returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+}
